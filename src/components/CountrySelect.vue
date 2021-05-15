3784205a09e59b27547547e926cbc68fca7b2f1f
aliabb01 <aliabbasov21112001@gmail.com>
2021-05-15 19:54:08 +0400

    Components added: Header, DataTitle, DataBoxes, CountrySelect

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <select style="outline:none;" @change="onChange()" v-model="selected" class="form-select mt-1 block p-3 rounded text-center">
+      <option value="0" disabled>Select Country</option>
+      <option v-for="country in countries" :key="country.ID" :value="country.ID">
+          {{ country.Country }}
+      </option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        onChange() {
+            const country = this.countries.find((item) => item.ID === this.selected)
+
+            this.$emit('get-country', country)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
